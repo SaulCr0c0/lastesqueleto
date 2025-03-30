39b7cdbcf043ed99ecc0bfc186f3446820e5b347
Saul <saulcordovavill@gmail.com>
2025-03-30 13:11:57 -0400

    add uses permision internet

--- app/src/main/java/com/spokefan/lastesqueleto/MainActivity.kt
+++ app/src/main/java/com/spokefan/lastesqueleto/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import com.spokefan.lastesqueleto.ui.theme.LastesqueletoTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +21,6 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LastesqueletoTheme {
-
             }
         }
     }
