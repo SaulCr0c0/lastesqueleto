1b47019ca3857bfeec581ed7cfc5ed7ef7a10f50
Saul <saulcordovavill@gmail.com>
2025-05-05 14:22:05 -0400

    create structure of screens

--- app/src/main/java/com/spokefan/lastesqueleto/ui/screens/homeScreen/HomeScreen.kt
+++ app/src/main/java/com/spokefan/lastesqueleto/ui/screens/homeScreen/HomeScreen.kt
@@ -1,21 +1,19 @@
 package com.spokefan.lastesqueleto.ui.screens.homeScreen
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun HomeScreen(
+fun HomeScreen(){
 
-){
-  Box(
+}
 
-  ){
-      Text(text = "HOLA USUARIO")
+@Composable
+fun HomeScreenContent(){
 
-  }
+}
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview(){
+
 }
--- app/src/main/java/com/spokefan/lastesqueleto/ui/screens/listScreen/ListScreen.kt
+++ app/src/main/java/com/spokefan/lastesqueleto/ui/screens/listScreen/ListScreen.kt
@@ -1,8 +1,20 @@
 package com.spokefan.lastesqueleto.ui.screens.listScreen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ListScreen(){
+
+}
+
+@Composable
+fun ListScreenContent(){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ListScreenPreview(){
 
 }
