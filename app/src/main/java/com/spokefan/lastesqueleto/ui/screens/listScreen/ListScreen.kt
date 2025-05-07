9f37dfbdaadcc6616de11b6ecc25c00a7dca4b7f
Saul <saulcordovavill@gmail.com>
2025-05-07 09:37:39 -0400

    create funtion postCard

--- app/src/main/java/com/spokefan/lastesqueleto/ui/screens/listScreen/ListScreen.kt
+++ app/src/main/java/com/spokefan/lastesqueleto/ui/screens/listScreen/ListScreen.kt
@@ -1,10 +1,20 @@
 package com.spokefan.lastesqueleto.ui.screens.listScreen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.spokefan.lastesqueleto.data.server.models.Post
 
 @Composable
-fun ListScreen(){
+fun ListScreen(
+
+){
 
 }
 
@@ -17,4 +27,22 @@ fun ListScreenContent(){
 @Composable
 fun ListScreenPreview(){
     ListScreenContent()
+}
+
+@Composable
+fun PostCard(post:Post){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Column(
+
+        ) {
+            Text(text = "ID User: ${post.userId}")
+            Text(text = "ID: ${post.id}")
+            Text(text = "Tittle: ${post.title}")
+            Text(text = "Body: ${post.body}")
+        }
+    }
 }
